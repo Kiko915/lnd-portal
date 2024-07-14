653efb1044300fbc56f78f8faef44766a5d3f76d
kikodevelops <kikoneilmistica@gmail.com>
2024-07-14 14:48:58 +0800

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import InDevelopment from "@/components/appComponents/InDevelopment";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <InDevelopment />
+    </main>
+  );
+}
---
+++ components/appComponents/InDevelopment.tsx
@@ -0,0 +1,15 @@
+export default function InDevelopment() {
+    return (
+        <>
+        <div className="min-h-screen flex items-center justify-center flex-col">
+            <div className="bg-gray-800 text-white px-8 py-2 rounded-md mb-4 shadow">
+                <p>LND PORTAL</p>
+            </div>
+            <h2 className="text-xl font-bold">In Development. 🏗️</h2>
+        </div>
+        <div className="bg-gray-100 p-2 text-sm text-center">
+            <span>Developed by DheKode Systems.</span>
+        </div>
+        </>
+    )
+}
